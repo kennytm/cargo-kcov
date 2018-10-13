572d69735e3d90ab9e7c6037ea749c420de59689
Samuele Maci <macisamuele@gmail.com>
2018-10-13 11:42:53 +0200

    Make number of parallel jobs configurable via env variable instead of using all CPUs all the time

--- src/install_kcov.sh
+++ src/install_kcov.sh
@@ -21,11 +21,18 @@ mkdir kcov-${KCOV_VERSION}
 curl -L --retry 3 "${KCOV_TGZ}" | tar xzvf - -C kcov-${KCOV_VERSION} --strip-components 1
 
 num_proc=1
+# If PARALLEL_BUILD environment variable is set then parallel build is enabled
 if [ "${PARALLEL_BUILD:-}" != "" ]; then
-    if command_exists nproc; then
-        num_proc=$(nproc)
-    elif command_exists sysctl; then
-        num_proc=$(sysctl -n hw.ncpu)
+    # If PARALLEL_BUILD content is a number then use it as number of parallel jobs
+    if [ ! -z "${PARALLEL_BUILD##*[!0-9]*}" ]; then
+        num_proc=${PARALLEL_BUILD}
+    else
+        # Try to determine the number of available CPUs
+        if command_exists nproc; then
+            num_proc=$(nproc)
+        elif command_exists sysctl; then
+            num_proc=$(sysctl -n hw.ncpu)
+        fi
     fi
 fi
 
